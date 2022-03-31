fcfda8159f26422fb3eb3d67985c0d837a5ca0dd
Isaiah Bullard <isaiah0812@yahoo.com>
2022-03-30 21:17:26 -0500

    add create beat endpoint

--- src/beats/index.ts
+++ src/beats/index.ts
@@ -5,30 +5,61 @@
 import express, { Request, Response } from 'express';
 import { getDb } from '../db/setup';
 import { checkJwt } from '../config/auth';
+import { Document, WithoutId } from 'mongodb';
+import Beat from '../models/beat';
+import { CreateError, InternalServerError, ValidationError } from '../models/errorHandling';
 const { default: logger } = require('../config/logger');
 
 const db = getDb()
 const router = express.Router();
 const beats = db.collection('beats');
 
-// TODO: POST / (add a beat)
 router.route('/')
   .get((req: Request, res: Response) => {
     logger.info(`Getting all beats for request from ${req.ip}`);
-    beats.find().toArray((error, result) => {
-      if(error) throw error
+    beats.find({}, { projection: { "_id": false }}).toArray((error: any, result: WithoutId<Document>[] | undefined) => {
+      if(error) {
+        logger.error("Error retrieving beats from db...");
+        res.status(500).json(new InternalServerError(error));
+        throw error
+      }
 
       res.json(result)
     })
   })
-  .post(checkJwt, (req: Request, res: Response) => {
-    res.send(req.body);
+  .post(checkJwt, async (req: Request, res: Response) => {
+    try {
+      const beat: Beat = new Beat(req.body);
+      const existingBeat: Document | null = await beats.findOne({ id: beat.id })
+
+      if (existingBeat) {
+        logger.warn(`Beat with id ${beat.id} already exists`);
+        throw new CreateError("beats", `Beat with id '${beat.id}' already exists. Use a different title, or send an 'id' field with the request with a unique id for the beat`)
+      } else {
+        logger.info(`Creating beat with the id ${beat.id}...`);
+        await beats.insertOne(beat);
+        logger.info(`Beat '${beat.id}' created!`);
+        res.send(beat as Beat);
+      }
+    } catch (e: any) {
+      if (e instanceof ValidationError) {
+        logger.error(e.message)
+        res.status(400).json(e)
+      } else if (e instanceof CreateError) {
+        logger.warn(e.message)
+        res.status(400).json(e)
+      } else {
+        logger.error(e.stack)
+        res.status(500).json(new InternalServerError(e))
+      }
+    }
   })
 
 /**
  * TODO: 
  * - GET /:beatId (fetch a beat)
  * - PUT /:beatId (update a beat)
+ * - DELETE /:beatId (delete a beat)
  */
 
 module.exports = router;
---
+++ src/models/beat.ts
@@ -0,0 +1,64 @@
+import { ZaetabaseDocument } from ".";
+import { KeySignatures } from "../utils/enums";
+import { ID_REGEX, NON_ID_CHARACTER_REGEX } from "../utils/helpers";
+import { COVER_FIELD_NAME, ID_FIELD_ERROR_MESSAGE, ID_FIELD_NAME, URL_FIELD_ERROR_MESSAGE, ValidationError } from "./errorHandling";
+
+type BeatType = {
+  id?: string;
+  title: string;
+  cover: URL;
+  youtube: string;
+  keySignature: KeySignatures;
+  tempo: number;
+  sold: boolean;
+}
+
+export default class Beat implements ZaetabaseDocument {
+  id: string;
+  title: string;
+  cover: URL;
+  youtube: string;
+  keySignature: KeySignatures;
+  tempo: number;
+  sold: boolean;
+
+  constructor(beat: BeatType) {
+    this.title = beat.title;
+    this.youtube = beat.youtube;
+    this.tempo = beat.tempo;
+    this.sold = beat.sold || false;
+
+    if (Object.values(KeySignatures).includes(beat.keySignature)) {
+      this.keySignature = beat.keySignature;
+    } else {
+      throw new ValidationError("keySignature", "Field must be a valid KeySignature");
+    }
+    
+    try {
+      this.cover = new URL(beat.cover);
+    } catch (e: any) {
+      if (e instanceof TypeError) {
+        throw new ValidationError(COVER_FIELD_NAME, URL_FIELD_ERROR_MESSAGE);
+      } else {
+        throw e;
+      }
+    }
+
+    if (beat.id) {
+      if (beat.id.match(ID_REGEX)) {
+        this.id = beat.id;
+      } else {
+        throw new ValidationError(ID_FIELD_NAME, ID_FIELD_ERROR_MESSAGE);
+      }
+    } else {
+      this.id = this.generateId();
+    }
+  }
+
+  generateId(): string {
+    return this.title
+      .toLowerCase()
+      .replace(/ /gi, "-")
+      .replace(NON_ID_CHARACTER_REGEX, "");
+  }
+}
