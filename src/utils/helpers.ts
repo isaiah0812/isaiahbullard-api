2fc2e1f44f3a2d261a2c476e5e58cd46fe8d1fce
Isaiah Bullard <isaiah0812@yahoo.com>
2022-03-30 21:13:34 -0500

    add regex constants to helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,3 +4,7 @@ export const printJSON = (jsonObject: any) => {
       ? Number.parseInt(value.toString())
       : value, 2)
 }
+
+export const NON_ID_CHARACTER_REGEX = /[\x21-\x2C\x2E-\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]/gi;
+
+export const ID_REGEX = /^[a-z0-9-]*$/gi;
