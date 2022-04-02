4b382937250fdb22e32f26b33848f4a90fd5400d
Isaiah Bullard <isaiah0812@yahoo.com>
2022-04-02 13:36:20 -0500

    add create merch item endpoint

--- src/merch/index.ts
+++ src/merch/index.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
+import { Document } from 'mongodb';
+import { checkJwt } from '../config/auth';
 import { getDb } from '../db/setup';
+import { CreateError, InternalServerError, ValidationError } from '../models/errorHandling';
+import Merch from '../models/merch';
 
 const { default: logger } = require('../config/logger');
 const db = getDb();
@@ -19,7 +23,34 @@ router.route('/')
 
       res.json(result)
     })
-  });
+  })
+  .post(checkJwt, async (req: Request, res: Response) => {
+    try {
+      const merchItem: Merch = new Merch(req.body);
+      const existingMerchItem: Document | null = await merch.findOne({ id: merchItem.id })
+
+      if (existingMerchItem) {
+        logger.warn(`Merch with id ${merchItem.id} already exists`);
+        throw new CreateError("merch", `Merch with id '${merchItem.id}' already exists. Use a different name, or send an 'id' field with the request with a unique id for the merch item`)
+      } else {
+        logger.info(`Creating merch item with the id ${merchItem.id}...`);
+        await merch.insertOne(merchItem);
+        logger.info(`Merch '${merchItem.id}' created!`);
+        res.send(merchItem as Merch);
+      }
+    } catch (e: any) {
+      if (e instanceof ValidationError) {
+        logger.error(e.message)
+        res.status(400).json(e)
+      } else if (e instanceof CreateError) {
+        logger.warn(e.message)
+        res.status(400).json(e)
+      } else {
+        logger.error(e.stack)
+        res.status(500).json(new InternalServerError(e))
+      }
+    }
+  })
 
 /**
  * TODO:
---
+++ src/models/merch.ts
@@ -0,0 +1,145 @@
+import { ZaetabaseDocument } from "."
+import { ID_REGEX, NON_ID_CHARACTER_REGEX } from "../utils/helpers";
+import { ID_FIELD_ERROR_MESSAGE, ID_FIELD_NAME, URL_FIELD_ERROR_MESSAGE, ValidationError } from "./errorHandling";
+
+const PICS_FIELD_NAME = "pics";
+const PICS_FIELD_ERROR_MESSAGE = "Items in array must be valid URL strings";
+
+type SizeType = {
+  id?: string,
+  name: string,
+  quantity: number,
+  pics?: URL[],
+  weight?: number,
+  price: number
+}
+
+export class Size implements ZaetabaseDocument {
+  id: string;
+  name: string;
+  quantity: number;
+  pics?: URL[];
+  weight?: number;
+  price: number;
+
+  constructor(size: SizeType) {
+    this.name = size.name;
+    this.quantity = size.quantity;
+    this.pics = size.pics;
+    this.weight = size.weight;
+    this.price = size.price;
+
+    if(size.pics) {
+      try {
+        this.pics = size.pics
+      } catch(e: any) {
+        if (e instanceof TypeError) {
+          throw new ValidationError(PICS_FIELD_NAME, PICS_FIELD_ERROR_MESSAGE);
+        } else {
+          throw e;
+        }
+      }
+    }
+
+    if (size.id) {
+      if(size.id.match(ID_REGEX)) {
+        this.id = size.id;
+      } else {
+        throw new ValidationError(ID_FIELD_NAME, ID_FIELD_ERROR_MESSAGE);
+      }
+    } else {
+      this.id = this.generateId();
+    }
+  }
+
+  generateId(): string {
+      return this.name
+        .toLowerCase()
+        .replace(/ /gi, "-")
+        .replace(NON_ID_CHARACTER_REGEX, "");
+  }
+}
+
+type MerchType = {
+  id?: string,
+  name: string,
+  description: string,
+  thumbnail: URL,
+  pics?: URL[],
+  sizes?: Size[],
+  price?: number,
+  weight?: number,
+  quantity?: number,
+}
+
+export default class Merch implements ZaetabaseDocument {
+  id: string;
+  name: string;
+  description: string;
+  thumbnail: URL;
+  pics?: URL[];
+  sizes?: Size[];
+  price?: number;
+  weight?: number;
+  quantity?: number;
+
+  constructor(merch: MerchType) {
+    this.name = merch.name;
+    this.description = merch.description;
+    this.sizes = merch.sizes;
+    this.price = merch.price;
+    this.weight = merch.weight;
+    this.quantity = merch.quantity;
+
+    if(merch.pics) {
+      try {
+        this.pics = merch.pics;
+      } catch(e: any) {
+        if (e instanceof TypeError) {
+          throw new ValidationError(PICS_FIELD_NAME, PICS_FIELD_ERROR_MESSAGE);
+        } else {
+          throw e;
+        }
+      }
+    }
+
+    if(merch.sizes) {
+      try {
+        this.sizes = merch.sizes;
+      } catch(e: any) {
+        if (e instanceof ValidationError) {
+          throw new ValidationError(`sizes.${e.field}`, e.message)
+        } else {
+          throw e;
+        }
+      }
+    }
+
+    try {
+      this.thumbnail = merch.thumbnail;
+    } catch (e: any) {
+      if (e instanceof TypeError) {
+        throw new ValidationError("thumbnail", URL_FIELD_ERROR_MESSAGE)
+      } else {
+        throw e;
+      }
+    }
+
+    if(merch.id) {
+      if (merch.id.match(ID_REGEX)) {
+        this.id = merch.id;
+      } else {
+        throw new ValidationError(ID_FIELD_NAME, ID_FIELD_ERROR_MESSAGE);
+      }
+    } else {
+      this.id = this.generateId();
+    }
+  }
+
+  generateId(): string {
+    return this.name
+      .toLocaleLowerCase()
+      .replace(/ /gi, "-")
+      .replace(NON_ID_CHARACTER_REGEX, "");
+  }
+}
